c7dc2e17441a2f938f97e859e642e52bcc806964
Mahit Mehta <mahitmehta@Mahits-MacBook-Air-604.local>
2022-03-29 16:42:47 -0400

    Updated Navbar Functionality

--- components/Navbar/index.tsx
+++ components/Navbar/index.tsx
@@ -1,11 +1,52 @@
 import Link from "next/link";
-import React, { useState } from "react";
+import React, { useEffect, useMemo, useState } from "react";
 import Image from "next/image";
 import { MenuIcon, XIcon } from "@heroicons/react/outline";
 import Button from "../Button";
+import { useRouter } from "next/router";
+import clsx from "clsx";
+
+interface INavbarRoute {
+    route: string; 
+    currentRoute: string; 
+    label: string; 
+}
+
+const NavbarRoute : React.FC<INavbarRoute> = ({ route, currentRoute, label }) => {
+    const selected = useMemo(() => route === currentRoute, [ route, currentRoute ]);
+
+    return (
+        <li>
+            <Link href={route} passHref>
+                <a 
+                    className={
+                        clsx(`block py-2 pr-4 pl-3`, 
+                            selected ? 
+                                `text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 dark:text-white` :
+                                `text-gray-700 border-b border-gray-100 hover:bg-gray-50 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700`
+                        )
+                    }
+                    aria-current="page">
+                    {label}
+                </a>
+            </Link>
+        </li>
+    )
+}
 
 const Navbar = () => {
     const [ mobileMenuOpen, setMobileMenuOpen ] = useState(false);
+    const [ currentRoute, setCurrentRoute  ] = useState("");
+    const router = useRouter();
+    
+    useEffect(() => {
+        router.events.on("hashChangeComplete", () => {
+            setCurrentRoute(`${window.location.pathname}${window.location.hash}`);
+        });
+        router.events.on("routeChangeComplete", () => {
+            setCurrentRoute(`${window.location.pathname}${window.location.hash}`);
+        });
+    }, [ router.events ]);
 
     return (
         <nav className={`fixed z-50 w-full ${mobileMenuOpen && "pb-[35px]"} md:pb-3 bg-white border-b border-solid border-b-gray-800 px-3 sm:px-4 py-2.5 dark:bg-gray-900 backdrop-blur-md dark:bg-opacity-75`}>
@@ -35,21 +76,9 @@ const Navbar = () => {
             </div>
             <div className={`${mobileMenuOpen ? "block" : "hidden"} justify-between items-center w-full md:flex md:w-auto md:order-1" id="mobile-menu-3`}>
                 <ul className="flex space-y-3 md:space-y-0 flex-col mt-4 md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium">
-                <li>
-                    <Link href={"/"} passHref>
-                        <a className="block py-2 pr-4 pl-3 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 dark:text-white" aria-current="page">Home</a>
-                    </Link>
-                </li>
-                <li>
-                    <Link href={"#about"} passHref>
-                        <a className="block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 md:dark:hover:text-white dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">Our Mission</a>
-                    </Link>
-                </li>
-                <li>
-                    <Link href={"#founder"} passHref>
-                        <a className="block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">Founder</a>
-                    </Link>
-                </li>
+                    <NavbarRoute label="Home" route="/" currentRoute={currentRoute}/>
+                    <NavbarRoute label="About" route="/#about" currentRoute={currentRoute}/>
+                    <NavbarRoute label="Founder" route="/#founder" currentRoute={currentRoute}/>
                 </ul>
             </div>
             </div>
