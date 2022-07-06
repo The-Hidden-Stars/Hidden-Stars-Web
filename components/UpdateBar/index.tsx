c722a193e2a6d206b05fca6aaad81fabebff4f5b
Mahit Mehta <mahit.py@gmail.com>
2022-07-06 11:35:11 -0400

    Style Fix

--- components/UpdateBar/index.tsx
+++ components/UpdateBar/index.tsx
@@ -6,7 +6,7 @@ const UpdateBar : React.FC<{}> = ({ children }) => {
     return (
         <div className={clsx(
             UpdateBarStyles.container, 
-            "relative p-3 space-x-2 border-b border-0 border-[rgba(255,255,255,0.1)] w-screen flex justify-center items-center"
+            "relative p-3 space-x-2 max-w-screen overflow-hidden border-b border-0 border-[rgba(255,255,255,0.1)] w-screen flex justify-center items-center"
         )}>
             { children }
         </div>
